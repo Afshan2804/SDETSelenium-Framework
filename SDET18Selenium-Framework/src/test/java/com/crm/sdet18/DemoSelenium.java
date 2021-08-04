bcc03eb432786095ceda51c2a9acd9bf5f6f6ea0
Afshan Firdose <Afshan Firdose@DESKTOP-S420HUE>
2021-08-04 13:55:06 +0530

    first commit

---
+++ SDET18Selenium-Framework/src/test/java/com/crm/sdet18/DemoSelenium.java
@@ -0,0 +1,11 @@
+package com.crm.sdet18;
+
+import org.testng.annotations.Test;
+
+public class DemoSelenium {
+	@Test
+	public void demoselenium() {
+		System.out.println("hi there i am in selenium framework");
+	}
+
+}
